58e10f6e4d9646b4c5a8cf3f43f7a70ad56d7989
Potipecan <podpecan.andraz28@gmail.com>
2021-02-08 16:31:37 +0100

    *export

---
+++ NEAT/RandomEvaluator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpNeat.Core;
+using SharpNeat.Phenomes;
+
+namespace A_NEAT_arena.NEAT
+{
+    class RandomEvaluator : IPhenomeEvaluator<IBlackBox>
+    {
+        public ulong EvaluationCount => 0;
+
+        public bool StopConditionSatisfied => false;
+
+        private Random Rand;
+
+        public RandomEvaluator()
+        {
+            Rand = new Random();
+        }
+
+        public FitnessInfo Evaluate(IBlackBox phenome)
+        {
+            int fit = Rand.Next() % 10000;
+            return new FitnessInfo(fit, fit);
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
