a7b1e73f66810f732b4918628a168589c5acc538
Potipecan <podpecan.andraz28@gmail.com>
2021-02-17 21:01:45 +0100

    + bundled settings class

---
+++ NEAT/BundledExperimentSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpNeat.EvolutionAlgorithms;
+using SharpNeat.Genomes.Neat;
+
+namespace A_NEAT_arena.NEAT
+{
+    public class BundledExperimentSettings
+    {
+        public int Population { get; private set; }
+        public int BatchSize { get; private set; }
+        public NeatEvolutionAlgorithmParameters EAParams { get; private set; }
+        public NeatGenomeParameters GenomeParams { get; private set; }
+
+        public BundledExperimentSettings(int population, int batchSize, NeatEvolutionAlgorithmParameters eaParams, NeatGenomeParameters genomeParams)
+        {
+            Population = population;
+            BatchSize = batchSize;
+            EAParams = eaParams;
+            GenomeParams = genomeParams;
+        }
+    }
+}
